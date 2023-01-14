c138f597e7c3f6f0d81ee2d83a13ab79b1b163ac
Subhradeep Ray (Nokia) <subhradeep.ray@nokia.com>
2023-01-14 16:24:05 +0530

    Remove UpperCase, LowerCase, Special Characters From A Given String

---
+++ Remove_Characters_From_String.java
@@ -0,0 +1,50 @@
+import java.util.regex.*;
+public class Main
+{
+	public static String removingUpperCaseCharacters(String str)
+	{
+		String regex = "[A-Z]";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(str);
+		return matcher.replaceAll("");
+	}
+	public static String removingLowerCaseCharacters(String str)
+	{
+		String regex = "[a-z]";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(str);
+		return matcher.replaceAll("");
+	}
+	public static String removingSpecialCharacters(String str)
+	{
+
+		String regex = "[^A-Za-z0-9]";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(str);
+		return matcher.replaceAll("");
+	}
+	public static String removingNumericCharacters(String str)
+	{
+        String regex = "[0-9]";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(str);
+		return matcher.replaceAll("");
+	}
+	public static String removingNonNumericCharacters(String str)
+	{
+		String regex = "[^0-9]";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(str);
+		return matcher.replaceAll("");
+	}
+	public static void main(String[] args)
+	{
+		String str = "Ab#45ER%6tg5$";
+
+		System.out.println("After Removing Uppercase Characters => " + removingUpperCaseCharacters(str));
+		System.out.println("After Removing Lowercase Characters => " + removingLowerCaseCharacters(str));
+		System.out.println("After Removing Special Characters => " + removingSpecialCharacters(str));
+		System.out.println("After Removing Numeric Characters => " + removingNumericCharacters(str));
+		System.out.println("After Removing Non-Numeric Characters => " + removingNonNumericCharacters(str));
+	}
+}
