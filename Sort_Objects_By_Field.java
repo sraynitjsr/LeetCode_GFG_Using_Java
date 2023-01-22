9067bed82114baffdc1d36111ea0175bb075ea9b
root <subhradeepray2017@gmail.com>
2023-01-22 18:38:32 +0530

    Custom Sort and Arrays Union, Intersection

---
+++ Sort_Objects_By_Field.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Student {
+    private int roll;
+    private String name;
+    
+    public Student(int r, String n) {
+        this.roll = r;
+        this.name = n;
+    }
+
+    public int getRoll() {
+        return this.roll;        
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
+class ComparatorImpl implements Comparator<Student> {
+    public int compare(Student s1, Student s2) {
+        return s2.getRoll() - s1.getRoll();
+    }
+}
+
+public class MyCustomSort {
+    public static void main(String[] args) {
+        Student s1 = new Student(10, "A");
+        Student s3 = new Student(30, "C");
+        Student s2 = new Student(20, "B");
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(s1);
+        students.add(s3);
+        students.add(s2);
+
+        System.out.println("Before Sorting");
+        students.forEach(student -> 
+            System.out.println(student.getName() + " " +  student.getRoll())
+        );
+
+        Collections.sort(students, new ComparatorImpl());
+
+        System.out.println("After Sorting");
+        students.forEach(student -> 
+        System.out.println(student.getName() + " " +  student.getRoll())
+        );
+    }
+}
