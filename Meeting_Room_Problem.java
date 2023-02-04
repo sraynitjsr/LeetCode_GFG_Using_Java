bba1fcf6ea5d2bd3a8b9086b1fdb8d7f7f9699fc
root <subhradeepray2017@gmail.com>
2023-02-04 19:17:04 +0530

    Maximum Meetings Possible

---
+++ Meeting_Room_Problem.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Meeting {
+    private int start;
+    private int end;
+    public Meeting(int a, int b) {
+        this.start = a;
+        this.end = b;
+    }
+    public int getStartTime() {
+        return this.start;
+    }
+
+    public int getEndTime() {
+        return this.end;
+    }
+}
+
+class MeetingComparator implements Comparator<Meeting> {
+    public int compare(Meeting a, Meeting b) {
+        return a.getEndTime() - b.getEndTime();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> startTimes = new ArrayList<>(Arrays.asList(1, 3, 0, 5, 8, 5));
+        ArrayList<Integer> endTimes = new ArrayList<>(Arrays.asList(2, 4, 6, 7, 9, 9));
+        
+        ArrayList<Meeting> meetings = new ArrayList<>();
+        
+        for(int i = 0; i < startTimes.size(); i++) {
+            Meeting newMeeting = new Meeting(startTimes.get(i), endTimes.get(i));
+            meetings.add(newMeeting);
+        }
+        
+        Collections.sort(meetings, new MeetingComparator());
+
+        int endTime = meetings.get(0).getEndTime();
+
+        System.out.println("Possible Meeting Timings Are");
+
+        for(int i = 1; i < meetings.size(); i++) {
+            if(meetings.get(i).getStartTime() > endTime){
+                System.out.println(meetings.get(i).getStartTime() + " to " + meetings.get(i).getEndTime());
+                endTime = meetings.get(i).getEndTime();
+            }
+        }
+    }
+}
