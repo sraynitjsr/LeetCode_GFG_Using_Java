80534697d368f33cd05219c3b9db80e0be67ac29
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-02-05 18:14:43 +0530

    Dutch National Flag Problem

---
+++ 18_Dutch_National_Flag_Problem.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void customSort(int arr[], int size) {
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
+        for(int i = 0; i < size; i++) {
+            if(arr[i] == 0) {
+                countZero++;
+            }
+            if(arr[i] == 1) {
+                countOne++;
+            }
+            if(arr[i] == 2) {
+                countTwo++;
+            }
+        }
+        int index = 0;
+        while(countZero > 0) {
+            arr[index] = 0;
+            countZero--;
+            index++;
+        }
+        while(countOne > 0) {
+            arr[index] = 1;
+            countOne--;
+            index++;
+        }
+        while(countTwo > 0) {
+            arr[index] = 2;
+            countTwo--;
+            index++;
+        }
+        System.out.print("Sorted Array is => ");
+        for(int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String args[]) {
+        int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+        int n = arr.length;
+        customSort(arr, n);
+    }
+}
