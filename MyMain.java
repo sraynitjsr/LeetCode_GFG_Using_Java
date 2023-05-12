35a99ba03307401b178f9297b56c26a7f28f60dc
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-12 21:00:06 +0530

    DutchNationalFlag

--- MyMain.java
+++ MyMain.java
@@ -1,5 +1,6 @@
 class MyMain {
     public static void main(String[] args) {
-        System.out.println("Solving DSA Questions of GFG and LeetCode Using Core Java (J2SE)"); 
+        System.out.println("Solving DSA Questions of GFG and LeetCode Using Core Java (J2SE)");
+        DutchNationalFlag.start();
     }
 }
