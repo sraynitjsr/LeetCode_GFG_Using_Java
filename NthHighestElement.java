839288575ffe47945e8f0d8c631d36ccf2f458a2
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-05-19 23:26:30 +0530

    NthHighestElement PriorityQueue

--- NthHighestElement.java
+++ NthHighestElement.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class NthHighestElement {
     public static void start() {
+        System.out.println("Nth Highest Element Problem");
         int n = 3; // Here we want third highest element
         
         List<Integer> input = Arrays.asList(55, 66, 77, 99, 88, 33, 11, 44, 22);
