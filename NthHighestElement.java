d0636f356e33d65dbe0f18b467c2bbe1fcaf7ae7
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-08-07 23:50:52 +0530

    Finding Nth Highest Element in Array Using Java

--- NthHighestElement.java
+++ NthHighestElement.java
@@ -1,1 +1,22 @@
+import java.util.*;
 
+public class NthHighestElement {
+    public static void start() {
+        System.out.println("Nth Highest Element Problem");
+        int n = 3;
+        
+        List<Integer> input = Arrays.asList(55, 66, 77, 99, 88, 33, 11, 44, 22);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i = 0; i < input.size(); i++) {
+            pq.add(input.get(i));
+        }
+        
+        for (int i = 0; i < n - 1; i++) {
+            pq.poll();
+        }
+        
+        System.out.println(pq.peek() + " is the " + n + "rd highest element in the array");
+    }
+}
