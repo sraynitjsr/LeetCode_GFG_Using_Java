dab374f1bb492fa3dfcb9e2e066223863c670f74
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-06-22 22:44:22 +0530

    Cycle Detection Using Floyd's Cycle Detection Algorithm

--- Blind75/LinkedListCycleDetection.java
+++ Blind75/LinkedListCycleDetection.java
@@ -11,6 +11,25 @@ public class LinkedListCycleDetection {
         }
     }
     
+    public static boolean hasCycleFloyd(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return true;
+    }
+    
     public static boolean hasCycleHashing(ListNode head) {
         HashSet<ListNode> visited = new HashSet<>();
         
@@ -30,7 +49,10 @@ public class LinkedListCycleDetection {
         head.next = new ListNode(2);
         head.next.next = new ListNode(0);
         head.next.next.next = new ListNode(-4);
-        head.next.next.next.next = head.next;   
+        head.next.next.next.next = head.next;
+        
+        boolean hasCycleFloyd = hasCycleFloyd(head);
+        System.out.println(hasCycleFloyd);
         
         boolean hasCycleHashing = hasCycleHashing(head);
         System.out.println(hasCycleHashing);
